8ff5b87a01abbc2e1df5ce738187beba549eb324
Rachmtbs <81941550+Rachmtbs@users.noreply.github.com>
2022-06-20 11:35:54 -0700

    just a beginner practice

---
+++ webdesign/app.js
@@ -0,0 +1,8 @@
+const menu= document.querySelector('#mobile-menu')
+const menulinks =document.querySelector ('.navbar__menu')
+menu.addEventListener('click', funcution())
+ {
+  menu.classList.toggle('is active') 
+   menulinks.classList.toggle  ('active');
+}
+
